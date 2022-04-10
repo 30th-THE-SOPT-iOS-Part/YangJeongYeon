0682ee947b4f22847f34b2b43cfc2a9dd7a9a187
jeongkite <jeong6kite@gmail.com>
2022-04-10 20:42:18 +0900

    ➕[ADD] 페이지별 viewcontroller 추가 (#1)
    Login, FirstSignup, SecondSignup, Welcome

---
+++ Instagram-CloneCoding/Instagram-CloneCoding/SecondSignupViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondSignupViewController.swift
+//  Instagram-CloneCoding
+//
+//  Created by 정연 on 2022/04/10.
+//
+
+import UIKit
+
+class SecondSignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
