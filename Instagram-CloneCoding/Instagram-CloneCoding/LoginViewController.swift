b7e4473e4d54a164be8075493d5954cde57f8154
jeongkite <jeong6kite@gmail.com>
2022-04-10 21:02:56 +0900

    ➕[ADD] Outlet 및 Action 연결 (#1)
    각 페이지별 필요한 버튼이랑 텍스트필드 연결해줌~~

--- Instagram-CloneCoding/Instagram-CloneCoding/LoginViewController.swift
+++ Instagram-CloneCoding/Instagram-CloneCoding/LoginViewController.swift
@@ -8,22 +8,19 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func tapLoginBtn(_ sender: Any) {
+    }
+    
     /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
-
 }
--- Instagram-CloneCoding/Instagram-CloneCoding/WelcomeViewController.swift
+++ Instagram-CloneCoding/Instagram-CloneCoding/WelcomeViewController.swift
@@ -9,21 +9,20 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func tapFinishBtn(_ sender: Any) {
     }
     
-
     /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
-
 }
