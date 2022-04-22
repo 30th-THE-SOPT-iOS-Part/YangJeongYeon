819645d3bd25d8cfc4485e05a052b3a81f461823
jeongkite <jeong6kite@gmail.com>
2022-04-22 23:46:11 +0900

    ✨[FEAT] 다른계정 로그인 버튼 기능 구현 (#3)
    좀 애매하긴 하다.. 수정해야함!!

--- Instagram-CloneCoding/Instagram-CloneCoding/AccountSetup/WelcomeViewController.swift
+++ Instagram-CloneCoding/Instagram-CloneCoding/AccountSetup/WelcomeViewController.swift
@@ -22,11 +22,21 @@ class WelcomeViewController: UIViewController {
         let mainTabBarStoryboard = UIStoryboard(name: "MainTabBar", bundle: nil)
         let mainTabBar = mainTabBarStoryboard.instantiateViewController(withIdentifier: "MainTabBar") as! UITabBarController
         /*
-         구글링해보니 AppDelegate 이용해서 바꾸는 방법도 있는데 window가 없다고 오류난다
+         [?] 구글링해보니 AppDelegate 이용해서 바꾸는 방법도 있는데 window가 없다고 오류난다
          let appDelegate = UIApplication.shared.delegate as! AppDelegate
          appDelegate.window?.rootViewController = mainTabBar
          */
         self.view.window?.rootViewController = mainTabBar
+    }
+    
+    /*
+     [TODO] 이거 가입하기 하고 누르면 로그인 화면이 아니라 비밀번호 설정 화면으로 넘어간다.
+     */
+    @IBAction func tapOtherLoginBtn(_ sender: Any) {
+        guard let presentViewController = presentingViewController as? UINavigationController else { return }
+        dismiss(animated: true) {
+            presentViewController.navigationController?.popToRootViewController(animated: true)
+        }
     }
     
     private func setUsername() {
