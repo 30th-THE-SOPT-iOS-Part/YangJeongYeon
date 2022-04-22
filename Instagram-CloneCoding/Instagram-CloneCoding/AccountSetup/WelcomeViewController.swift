e3eecf003ab3dc6b398373c15935028a248c95ea
jeongkite <jeong6kite@gmail.com>
2022-04-22 21:44:11 +0900

    ✨[FEAT] 완료버튼 클릭시 탭바로 이동 (#3)

--- Instagram-CloneCoding/Instagram-CloneCoding/AccountSetup/WelcomeViewController.swift
+++ Instagram-CloneCoding/Instagram-CloneCoding/AccountSetup/WelcomeViewController.swift
@@ -19,7 +19,14 @@ class WelcomeViewController: UIViewController {
     }
 
     @IBAction func tapFinishBtn(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
+        let mainTabBarStoryboard = UIStoryboard(name: "MainTabBar", bundle: nil)
+        let mainTabBar = mainTabBarStoryboard.instantiateViewController(withIdentifier: "MainTabBar") as! UITabBarController
+        /*
+         구글링해보니 AppDelegate 이용해서 바꾸는 방법도 있는데 window가 없다고 오류난다
+         let appDelegate = UIApplication.shared.delegate as! AppDelegate
+         appDelegate.window?.rootViewController = mainTabBar
+         */
+        self.view.window?.rootViewController = mainTabBar
     }
     
     private func setUsername() {
---
+++ Instagram-CloneCoding/Instagram-CloneCoding/MainTabBar/MainTabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  MainTabBarController.swift
+//  Instagram-CloneCoding
+//
+//  Created by 정연 on 2022/04/22.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
