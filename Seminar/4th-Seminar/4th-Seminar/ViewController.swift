fe612a9372dead559bd9d043c42dcdf04af5ce7f
jeongkite <jeong6kite@gmail.com>
2022-05-07 14:52:35 +0900

    ➕[ADD] 4차 세미나 프로젝트 생성 (4th-Seminar)

---
+++ Seminar/4th-Seminar/4th-Seminar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  4th-Seminar
+//
+//  Created by 정연 on 2022/05/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
