0113b4a1a58bd66f3bef9fde5be121c1cc5245a6
jeongkite <jeong6kite@gmail.com>
2022-05-16 11:51:54 +0900

    ⚰️ [DEL] 오류 코드 삭제 (#7)

--- Instagram-CloneCoding/Instagram-CloneCoding/Network/APIConstants.swift
+++ Instagram-CloneCoding/Instagram-CloneCoding/Network/APIConstants.swift
@@ -10,10 +10,11 @@ import Foundation
 struct APIConstants {
     // MARK: Base URL
     static let baseURL = "http://13.124.62.236"
-    static let imageListURL = "https://picsum.photos/v2/list"
     
     // MARK: Auth URL
     static let signupURL = baseURL + "/auth/signup"
     static let loginURL = baseURL + "/auth/signin"
     
+    // MARK: Image URL
+    static let imageListURL = "https://picsum.photos/v2/list"
 }
--- Instagram-CloneCoding/Instagram-CloneCoding/Source/ViewController/Auth/WelcomeViewController.swift
+++ Instagram-CloneCoding/Instagram-CloneCoding/Source/ViewController/Auth/WelcomeViewController.swift
@@ -4,7 +4,6 @@
 //
 //  Created by 정연 on 2022/04/10.
 //
-
 import UIKit
 
 class WelcomeViewController: UIViewController {
@@ -32,7 +31,7 @@ class WelcomeViewController: UIViewController {
     }
     
     // MARK: Custom Methods
-    /// 로그인 성공시에 alert 띄우는 메서드
+    /// 회원가입 성공시에 alert 띄우는 메서드
     private func successAlert(name: String)
     {
         let alert = UIAlertController(title: "회원가입 성공", message: "\(name)님 환영합니다!", preferredStyle: .alert)
@@ -42,28 +41,9 @@ class WelcomeViewController: UIViewController {
                 presentingViewController.popToRootViewController(animated: true)}
         }
         alert.addAction(alertAction)
-        
-        ImageService.share.getImages() { response in
-            switch response {
-                case .success(let data):
-                    for(var i = 0; i<FeedDataModel.sampleData.count(); i++) {
-                        FeedDataModel.sampleData[i].contentImage = data[i].download_url
-                    }
-                    present(alert, animated: true)
-                case .requestErr(_):
-                    self.failAlert(errCode: "Request Error")
-                case .pathErr:
-                    self.failAlert(errCode: "Path Error")
-                case .serverErr:
-                    self.failAlert(errCode: "Server Error")
-                case .networkFail:
-                    self.failAlert(errCode: "Network Fail Error")
-                default:
-                    self.failAlert(errCode: "Error")
-            }
-        }
+        present(alert, animated: true)
     }
-    /// 로그인 실패시에 alert 띄우는 메서드
+    /// 회원가입 실패시에 alert 띄우는 메서드
     private func failAlert(errCode: String)
     {
         let alert = UIAlertController(title: "회원가입 실패", message: "다시 시도해보세요\n\(errCode)", preferredStyle: .alert)
@@ -72,6 +52,7 @@ class WelcomeViewController: UIViewController {
         present(alert, animated: true)
     }
     
+    /// View에 username 값을 채워넣는 메서드
     private func setUsername() {
         if let username = username {
             welcomeLabel.text = "\(username) 님 Instagram에\n오신 것을 환영합니다"
